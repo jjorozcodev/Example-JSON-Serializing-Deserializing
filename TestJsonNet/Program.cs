5b9b7c97773e94ac5dbd66ef52f179f515e6bafb
JJ Orozco López <jjorozcodev@gmail.com>
2020-01-08 00:16:49 -0600

    Methods Json to Obj / Obj to Json
    Serialization and Deserialization Methods and example with differents data types

--- TestJsonNet/Program.cs
+++ TestJsonNet/Program.cs
@@ -1,9 +1,47 @@
-﻿namespace TestJsonNet
+﻿using System;
+using Newtonsoft.Json;
+
+namespace TestJsonNet
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("USING NEWTONSOFT JSON.NET");
+            Console.WriteLine("*************************\n");
+
+            MyObject objExample = new MyObject();
+
+            objExample.intData = 13;
+            objExample.stringData = "Hey JJ!";
+            objExample.decimalData = 7.13m;
+            objExample.doubleData = 13.7;
+            objExample.datetimeData = DateTime.Now;
+            objExample.boolData = true;
+
+            string resultJson = SerializeMyObject(objExample);
+
+            Console.WriteLine("Json string result is> \n" + resultJson);
+
+            string JsonMock = "{\"intData\":101,\"stringData\":\"This is a Json!\",\"decimalData\":104.7,\"doubleData\":105.1,\"datetimeData\":\"2021-02-13T23:59:00.1234567-06:00\",\"boolData\":false}";
+
+            MyObject objMock = DeserializeJsonString(JsonMock);
+
+            Console.WriteLine("Data object result is> \nINT: " + objMock.intData + " STRING: " + objMock.stringData + " DECIMAL: " + objMock.decimalData + " DOUBLE: " + objMock.doubleData + " DATETIME: " + objMock.datetimeData + " BOOL: " + objMock.boolData + "\n\n");
+
+            Console.ReadKey();
+        }
+
+        private static string SerializeMyObject(MyObject myObject)
+        {
+            string outputString = JsonConvert.SerializeObject(myObject);
+            return outputString;
+        }
+
+        private static MyObject DeserializeJsonString(string Json)
+        {
+            MyObject resultObj = JsonConvert.DeserializeObject<MyObject>(Json);
+            return resultObj;
         }
     }
 }
